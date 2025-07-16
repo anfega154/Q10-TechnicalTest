731f64a3e4558e4d24bd88b748637d994fbba826
Andres Felipe Gañan Moreno <aganan@ADADOM.LOCAL>
2025-07-16 14:52:20 -0500

    initial commit

--- obj/Debug/net8.0/Q10-TechnicalTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/Q10-TechnicalTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Q10-TechnicalTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f9bbf88370b2f36bcd4118588647b68562716d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Q10-TechnicalTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Q10-TechnicalTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
